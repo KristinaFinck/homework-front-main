c308c4b227dbeca4857399e4819f0badd6ff4acc
KristinaFinck <kristana.finck83@gmail.com>
2023-09-18 13:48:01 +0300

    Исправлены ошибки в HW1.tsx и UserList.tsx

---
+++ It-Incubator/homework-front-main/src/HW/1/UserList.tsx
@@ -0,0 +1,73 @@
+
+type AddressType = {
+  street: string; // ПОДПРАВЛЯЕМ any - поправила
+  city: string; // ПОДПРАВЛЯЕМ any - поправила 
+};
+
+type UserType = {
+  // ПРИДЕТСЯ САМОМУ) - написала пропсы
+    id: number
+    name: string
+    age: number
+    address: AddressType
+};
+
+type UserListPropsType = {
+  users: UserType[]; // ПО МОЕМУ ЧЕГО-ТО НЕ ХВАТАЕТ... - добавила значок массива
+};
+
+export const UserList = (props: UserListPropsType) => {
+  return (
+    <div id={"hw01-users"}>
+      <h2>User List:</h2>
+      <ul>
+      {props.users.map((user) => (
+          <li key={user.id} id={`hw01-user-${user.id}`}>
+          <strong>{user.name}</strong>
+        (Age: {user.age})
+        <strong> Address:</strong>
+      {user.address.street}, {user.address.city}
+      </li>
+      ))}
+      </ul>
+    </div>
+  );
+}
+
+
+/*
+type AddressType = {
+    street: string; // ПОДПРАВЛЯЕМ any
+    city: string; // ПОДПРАВЛЯЕМ any
+};
+
+type UserType = {
+    // ПРИДЕТСЯ САМОМУ)
+    id: number
+    name: string
+    age: number
+    address: AddressType
+};
+
+type UserListPropsType = {
+    users: UserType[]; // ПО МОЕМУ ЧЕГО-ТО НЕ ХВАТАЕТ...
+};
+
+export const UserList = (props: UserListPropsType) => {
+    return (
+        <div id={'hw01-users'}>
+            <h2>User List:</h2>
+
+            <ul>
+                {props.users.map((user) => ( // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...
+                    <li key={user.id} id={`hw01-user-${user.id}`}>
+                        <strong>{user.name}</strong> (Age: {user.age})<strong> Address:</strong>
+                        {user.address.street}, {user.address.city}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+ */
